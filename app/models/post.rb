15d40e2924c4dca38b4c41739cdc52ac998cafba
Oscar Mier <voscarmv@gmail.com>
2020-04-22 17:19:15 -0500

    Fixed linter errors.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,21 +1,21 @@
 class Post < ApplicationRecord
   validates :title,
-    presence: {
-      message: "A post needs a title"
-    },
-    length:  {
-      maximum: 200,
-      too_long: "This is not the body of your article"
-    }
+            presence: {
+              message: 'A post needs a title'
+            },
+            length: {
+              maximum: 200,
+              too_long: 'This is not the body of your article'
+            }
 
   validates :body,
-    presence: {
-      message: "You can't have an empty post"
-    },
-    length: {
-      maximum: 20000,
-      too_long: "Your post is too big"
-    }
+            presence: {
+              message: "You can't have an empty post"
+            },
+            length: {
+              maximum: 20_000,
+              too_long: 'Your post is too big'
+            }
   belongs_to :user
   has_many :comment
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   validates :name,
-    presence: {
-      message: "A ha! Nameless? I don't think so."
-    },
-    length: {
-      in: 6..24,
-      too_short: "You fool! It's too short or long;"
-    }
+            presence: {
+              message: "A ha! Nameless? I don't think so."
+            },
+            length: {
+              in: 6..24,
+              too_short: "You fool! It's too short or long;"
+            }
   has_many :post
   has_many :comment
 end
